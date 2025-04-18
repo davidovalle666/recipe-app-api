6e7f6e911fca0597fb6187a7521d8b05d5e07205
dsovallep <dsovallep@unal.edu.co>
2025-04-18 11:49:19 -0500

    Instalation of Postgres db in docker

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+calculator functions
+
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract y from x and return result."""
+
+    return y - x
