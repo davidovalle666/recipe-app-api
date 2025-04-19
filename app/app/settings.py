76e1c1987be7fb14278a7e95c0cb73868727ad12
dsovallep <dsovallep@unal.edu.co>
2025-04-19 17:26:45 -0500

    Deploy and proxy config

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cuxla(04bl^!)#^m#gkpdqdj$gdlu36ay47$#f=-p=hvu18b*+'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0))) #set to false in production
+# 1 True, 0 False
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 
 # Application definition
